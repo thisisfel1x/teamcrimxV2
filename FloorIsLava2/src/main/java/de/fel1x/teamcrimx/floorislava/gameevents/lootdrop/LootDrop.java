f0c249b1bf700be17750553c543b1243aeebf72d
thisisfel1x <felixweber271@gmail.com>
2020-12-04 15:04:47 +0100

    FloorIsLava: added loot to LootDrop
    Took 24 minutes

--- FloorIsLava2/src/main/java/de/fel1x/teamcrimx/floorislava/gameevents/lootdrop/LootDrop.java
+++ FloorIsLava2/src/main/java/de/fel1x/teamcrimx/floorislava/gameevents/lootdrop/LootDrop.java
@@ -1,6 +1,7 @@
 package de.fel1x.teamcrimx.floorislava.gameevents.lootdrop;
 
 import de.fel1x.teamcrimx.crimxapi.utils.InstantFirework;
+import de.fel1x.teamcrimx.crimxapi.utils.ItemBuilder;
 import de.fel1x.teamcrimx.floorislava.FloorIsLava;
 import de.fel1x.teamcrimx.floorislava.utils.ArmorstandStatsLoader;
 import org.bukkit.*;
@@ -9,11 +10,15 @@ import org.bukkit.entity.Chicken;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Item;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionType;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class LootDrop {
@@ -23,6 +28,40 @@ public class LootDrop {
     private LootDropTier lootDropTier = LootDropTier.TIER_1;
     private double speed = 1.0;
     private Material blockType;
+    private final Random random = new Random();
+
+    private final ItemStack[] lootDropItemsTierOne = {
+            // BLOCKS
+            new ItemStack(Material.SANDSTONE, this.random.nextInt(20) + 20),
+            new ItemStack(Material.GREEN_STAINED_GLASS, this.random.nextInt(20) + 20),
+            new ItemStack(Material.BLUE_GLAZED_TERRACOTTA, this.random.nextInt(20) + 20),
+            new ItemStack(Material.PURPLE_CONCRETE, this.random.nextInt(20) + 20),
+            new ItemStack(Material.OAK_PLANKS, this.random.nextInt(20) + 20),
+
+            // FOOD
+            new ItemStack(Material.BAKED_POTATO, this.random.nextInt(5) + 2),
+            new ItemStack(Material.COOKED_MUTTON, this.random.nextInt(5) + 2),
+            new ItemStack(Material.COOKIE, this.random.nextInt(5) + 2),
+            new ItemStack(Material.CAKE),
+
+            // STUFF
+            new ItemStack(Material.OAK_SAPLING, this.random.nextInt(2) + 1),
+            new ItemStack(Material.FARMLAND, this.random.nextInt(6) + 5),
+            new ItemStack(Material.WHEAT_SEEDS, this.random.nextInt(4) + 2),
+            new ItemStack(Material.BONE, this.random.nextInt(4) + 2),
+            new ItemStack(Material.COBWEB, this.random.nextInt(4) + 2),
+            new ItemStack(Material.IRON_INGOT, this.random.nextInt(4) + 2),
+            new ItemStack(Material.SNOWBALL, this.random.nextInt(2) + 2),
+            new ItemStack(Material.WATER_BUCKET),
+            new ItemStack(Material.FLINT_AND_STEEL),
+
+            // POTION
+            new ItemBuilder(Material.SPLASH_POTION).setPotionEffect(PotionType.INSTANT_HEAL).toItemStack(),
+            new ItemBuilder(Material.SPLASH_POTION).setPotionEffect(PotionType.REGEN).toItemStack(),
+            new ItemBuilder(Material.SPLASH_POTION).setPotionEffect(PotionType.SLOW_FALLING).toItemStack(),
+            new ItemBuilder(Material.SPLASH_POTION).setPotionEffect(PotionType.INVISIBILITY).toItemStack(),
+            new ItemBuilder(Material.SPLASH_POTION).setPotionEffect(PotionType.FIRE_RESISTANCE).toItemStack(),
+    };
 
     public LootDrop(@NotNull FloorIsLava floorIsLava,@NotNull Location origin, Material material) {
         this.floorIsLava = floorIsLava;
@@ -107,9 +146,7 @@ public class LootDrop {
         dropLocation.getBlock().setType(Material.CHEST);
 
         Chest chest = (Chest) dropLocation.getBlock().getState();
-        chest.getBlockInventory().setItem(new Random().nextInt(chest.getBlockInventory().getSize() - 1), new ItemStack(Material.COCOA_BEANS, 3));
-        chest.getBlockInventory().setItem(new Random().nextInt(chest.getBlockInventory().getSize() - 1), new ItemStack(Material.WATER_BUCKET));
-        chest.getBlockInventory().setItem(new Random().nextInt(chest.getBlockInventory().getSize() - 1), new ItemStack(Material.SANDSTONE, 37));
+        this.fillChest(chest);
 
         chest.setCustomName("§aLootdrop");
 
@@ -122,5 +159,37 @@ public class LootDrop {
                 .trail(true).withFade(Color.GREEN, Color.RED, Color.BLUE, Color.YELLOW,
                         Color.AQUA, Color.ORANGE, Color.PURPLE, Color.MAROON, Color.FUCHSIA)
                 .build(), dropLocation.clone().add(0, 0.5, 0));
+    }
+
+    private void fillChest(Chest chest) {
+
+        ArrayList<ItemStack> itemsAsList = new ArrayList<>(Arrays.asList(this.lootDropItemsTierOne));
+        Collections.shuffle(itemsAsList);
+
+        boolean[] chosen = new boolean[chest.getBlockInventory().getSize()]; // This checks which slots are already taken in the inventory.
+
+        int loot = this.random.nextInt(3);
+        for (int i = 0; i < (3 + loot); i++) {
+
+            Inventory chestInv = chest.getBlockInventory();
+
+            int slot;
+
+            do {
+                slot = this.random.nextInt(chestInv.getSize());
+            } while (chosen[slot]); // Make sure the slot does not already have an item in it.
+
+            chosen[slot] = true;
+            ItemStack is = itemsAsList.get(this.random.nextInt(itemsAsList.size()));
+            Material current = is.getType();
+
+            while (chest.getBlockInventory().contains(current)) {
+                is = itemsAsList.get(this.random.nextInt(itemsAsList.size()));
+                current = is.getType();
+            }
+
+            chestInv.setItem(this.random.nextInt(chestInv.getSize()), is); // Set the item in the chest to a random place (which is not taken).
+        }
+
     }
 }
